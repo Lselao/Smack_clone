9196e194bf3242df4e287157057ca4b4d1e2dce4
Lselao <lselao@student.wethinkcode.co.za>
2020-05-14 23:07:10 +0200

    smack_clone

---
+++ Smack_clone/Smack_clone/Controller/ChannelVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChannelVC.swift
+//  Smack_clone
+//
+//  Created by lebose on 2020/05/14.
+//  Copyright © 2020 lebose. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ Smack_clone/Smack_clone/Controller/ChatVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChatVC.swift
+//  Smack_clone
+//
+//  Created by lebose on 2020/05/14.
+//  Copyright © 2020 lebose. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
